265ecd57f80f072cf3fd76fd44ed86ba4cda003c
Crowdbotics <team@crowdbotics.com>
2020-11-10 02:57:35 +0000

    Update models

--- backend/h2trak_22464/wsgi.py
+++ backend/h2trak_22464/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'h2trak_22464.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h2trak_22464.settings")
 
 application = get_wsgi_application()
